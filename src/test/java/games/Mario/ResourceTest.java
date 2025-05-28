b617e24af497f635bb3cd48571b801d32828456c
David Rothammel <david.rothammel.cp@renesas.com>
2025-05-28 14:42:25 +0200

    added resource tests for mario images

---
+++ src/test/java/games/Mario/ResourceTest.java
@@ -0,0 +1,73 @@
+package games.Mario;
+
+import javax.swing.ImageIcon;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import org.junit.jupiter.api.Test;
+
+public class ResourceTest {
+
+    @Test
+    public void testMarioImageResourceExists() {
+        java.net.URL url = ResourceTest.class.getClassLoader()
+                              .getResource("games/Mario/mario.png");
+        assertNotNull(url, "Die Ressource games/Mario/mario.png sollte im Klassenpfad liegen");
+    }
+
+    @Test
+    public void testMarioImageIconLoads() {
+        java.net.URL url = ResourceTest.class.getClassLoader()
+                              .getResource("games/Mario/mario.png");
+        assertNotNull(url, "Resource darf nicht null sein");
+        ImageIcon icon = new ImageIcon(url);
+        assertNotNull(icon.getImage(), "Das geladene Bild darf nicht null sein");
+    }
+
+    @Test
+    public void testPlayerImageResourceExists() {
+        java.net.URL url = ResourceTest.class.getClassLoader()
+                              .getResource("games/Mario/Player.png");
+        assertNotNull(url, "Die Ressource games/Mario/Player.png sollte im Klassenpfad liegen");
+    }
+
+    @Test
+    public void testPlayerImageIconLoads() {
+        java.net.URL url = ResourceTest.class.getClassLoader()
+                              .getResource("games/Mario/Player.png");
+        assertNotNull(url, "Resource games/Mario/Player.png darf nicht null sein");
+        ImageIcon icon = new ImageIcon(url);
+        assertNotNull(icon.getImage(), "Das geladene Player-Bild darf nicht null sein");
+    }
+
+    @Test
+    public void testFeuerblumeImageResourceExists() {
+        java.net.URL url = ResourceTest.class.getClassLoader()
+                              .getResource("games/Mario/Items/Feuerblume.png");
+        assertNotNull(url, "Die Ressource games/Mario/Items/Feuerblume.png sollte im Klassenpfad liegen");
+    }
+
+    @Test
+    public void testFeuerblumeImageIconLoads() {
+        java.net.URL url = ResourceTest.class.getClassLoader()
+                              .getResource("games/Mario/Items/Feuerblume.png");
+        assertNotNull(url, "Resource games/Mario/Items/Feuerblume.png darf nicht null sein");
+        ImageIcon icon = new ImageIcon(url);
+        assertNotNull(icon.getImage(), "Das geladene Feuerblume-Bild darf nicht null sein");
+    }
+
+    @Test
+    public void testFeuermarioImageResourceExists() {
+        java.net.URL url = ResourceTest.class.getClassLoader()
+                              .getResource("games/Mario/Feuermario.png");
+        assertNotNull(url, "Die Ressource games/Mario/Items/Feuermario.png sollte im Klassenpfad liegen");
+    }
+
+    @Test
+    public void testFeuermarioImageIconLoads() {
+        java.net.URL url = ResourceTest.class.getClassLoader()
+                              .getResource("games/Mario/Feuermario.png");
+        assertNotNull(url, "Resource games/Mario/Items/Feuermario.png darf nicht null sein");
+        ImageIcon icon = new ImageIcon(url);
+        assertNotNull(icon.getImage(), "Das geladene Feuermario-Bild darf nicht null sein");
+    }
+}
